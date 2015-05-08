d8a044d2f373268a3a62f46f66be57b078eea91e
Shilin Victor <chrono.monochrome@gmail.com>
2015-05-09 00:13:33 +0300

    omni initial bring up
    Change-Id: Id7049bc774cd868844ea23289d1a58eb47521e86
    Signed-off-by: Shilin Victor <chrono.monochrome@gmail.com>

--- omni_codina.mk
+++ omni_codina.mk
@@ -8,14 +8,13 @@ TARGET_SCREEN_WIDTH := 480
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/codina/codina.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_codina
+PRODUCT_NAME := omni_codina
 PRODUCT_DEVICE := codina
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I8160
