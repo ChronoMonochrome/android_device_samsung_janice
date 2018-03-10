dfe68eb5e253e6814e8ed5363c8118e059cd9cc1
Shilin Victor <chrono.monochrome@gmail.com>
2018-03-10 17:32:43 +0300

    Disable vibrator temporarily

--- janice.mk
+++ janice.mk
@@ -202,6 +202,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstlport
 
+# Disable vibrator by using not proper HAL
+PRODUCT_PACKAGES += \
+    vibrator.montblanc
+
 # Gralloc := \
     gralloc.montblanc
 
