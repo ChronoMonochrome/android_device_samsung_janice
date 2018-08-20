4e00aa9fbf9e2a1294e301b23f0891082624cd69
Shilin Victor <chrono.monochrome@gmail.com>
2018-08-20 12:46:13 +0300

    Add libsensors
    based on https://github.com/Meticulus/android_device_huawei_hi6250/tree/master/libsensors
    
    Thanks to @Meticulus

---
+++ libsensors/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_CLANG := false
+LOCAL_CFLAGS := -DTAG="Sensors" -DSKIP_CXXRTOMB
+LOCAL_SRC_FILES := sensors.c
+
+include $(BUILD_SHARED_LIBRARY)
